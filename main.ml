a98f1731399e2a131fe4c204c59dc1c625dc44ef
Matias Goldfeld <2000matu@gmail.com>
2021-01-27 03:20:06 -0500

    Removed TTS and fixed kill timeout

--- main.ml
+++ main.ml
@@ -11,14 +11,14 @@ let wipe_sys =
   "module Sys = struct end \
    module Stdlib = struct include Stdlib module Sys = struct end end;;"
 
-let run_ocaml = Process.create ~prog:"/usr/bin/env"
-    ~args:["ocaml"; "-color"; "never"; "-stdin"; "-noinit"]
+let run_ocaml = Process.create ~prog:"/usr/bin/ocaml"
+    ~args:["-color"; "never"; "-stdin"; "-noinit"]
 
 let timeout_kill (ps : Process.t) (seconds : float) : unit =
   after (Time.Span.of_sec seconds) >>> fun () ->
   Process.send_signal ps Signal.kill
 
-let temp (message : Message.t) (code : string) : Message.t Deferred.Or_error.t =
+let run (message : Message.t) (code : string) : Message.t Deferred.Or_error.t =
   run_ocaml () >>= function
   | Error _ as e ->
     print_endline "Error creating new process";
@@ -27,6 +27,9 @@ let temp (message : Message.t) (code : string) : Message.t Deferred.Or_error.t =
     let stdin = Process.stdin ps in
     Writer.write_line stdin wipe_sys;
     Writer.write stdin code;
+      Process.pid ps
+  |> Pid.to_string
+  |> print_endline;
     timeout_kill ps 15.;
     let%bind output = Process.collect_output_and_wait ps in
     begin match output.exit_status with
@@ -43,16 +46,17 @@ let temp (message : Message.t) (code : string) : Message.t Deferred.Or_error.t =
         "\nstdout:```\n" ^ stdout ^ "\n```stderr:```\n" ^ stderr ^ "\n```"
     in
     if String.length reply_msg <= 2000 then
-      Message.reply_with ~tts:true ~content:reply_msg message
+      Message.reply_with ~content:reply_msg message
     else
-      Message.reply_with ~tts:true ~content:exit_msg ~files:[("output.txt", reply_msg)] message
+      Message.reply_with ~content:exit_msg ~files:[("output.txt", reply_msg)] message
 
 let check_command (message : Message.t) : unit =
   if Pcre.pmatch ~rex:prefix_pattern message.content then
     try
       let substrings = Pcre.exec ~rex:command_pattern message.content in
       let code = Pcre.get_substring substrings 2 in
-      temp message code >>> function
+      print_endline "Received valid command";
+      run message code >>> function
       | Ok _ -> print_endline "Success"
       | Error e -> Error.to_string_hum e |> print_endline
     with Caml.Not_found ->
